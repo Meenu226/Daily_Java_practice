ba20c7ef8e2a2814d754c8cdedfe6cb3339470e6
Meenu226 <meenutyagi226@gmail.com>
2026-02-12 11:02:20 +0530

    new line added

--- src/main/java/lab1.java
+++ src/main/java/lab1.java
@@ -1,6 +1,9 @@
+import javax.xml.transform.stream.StreamSource;
+
 public class lab1 {
     public static void main(String[]args){
         System.out.println("hello world");
+        System.out.println(10+20);
 
     }
 }
